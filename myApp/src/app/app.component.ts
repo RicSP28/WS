2ba787082c461029851727afeceb760261593b45
bdchere <57354102+bdchere@users.noreply.github.com>
2019-11-09 21:48:26 -0300

    atualizações no login

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -17,11 +17,6 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    },
-    {
       title: 'Login',
       url: '/login',
       icon: 'log-in'
