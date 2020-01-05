701c4bcbf2f87d7c37a27ea661e972c5b5474696
RicSP28 <46528082+RicSP28@users.noreply.github.com>
2020-01-05 18:34:51 -0300

    Prog fidelidade ,page

---
+++ prog-fidelidade.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prog-fidelidade',
+  templateUrl: './prog-fidelidade.page.html',
+  styleUrls: ['./prog-fidelidade.page.scss'],
+})
+export class ProgFidelidadePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
